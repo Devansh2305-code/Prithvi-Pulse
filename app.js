26898333fb40890af1f0be9c07ed4320bbc8f2e0
Devansh Jain <djdevansh.2305@gmail.com>
2026-04-02 11:23:05 +0530

    Update Google Form URLs to use 'usp=dialog'

--- app.js
+++ app.js
@@ -66,9 +66,9 @@ createParticles();
 ════════════════════════════════════════════ */
 // Replace the placeholder form IDs below with your actual Google Form embed URLs.
 const FORM_URLS = {
-  debate:  'https://docs.google.com/forms/d/e/1FAIpQLSe9uCVtqY79bSSGoRtObagwh4bHzZD2PZCK2G7q1a5LtvXJmA/viewform?usp=header',
-  fashion: 'https://docs.google.com/forms/d/e/1FAIpQLScPZJv5Iz9j0uzGd14oGlp_Ioq_4j5zMd_XmdGvI49Bf26jqw/viewform?usp=header',
-  poster:  'https://docs.google.com/forms/d/e/1FAIpQLSdI5gLIoP7CciJbT-btP24wmg21gD8ZDRblBbyUCF1mUFNQOA/viewform?usp=header',
+  debate:  'https://docs.google.com/forms/d/e/1FAIpQLSe9uCVtqY79bSSGoRtObagwh4bHzZD2PZCK2G7q1a5LtvXJmA/viewform?usp=dialog',
+  fashion: 'https://docs.google.com/forms/d/e/1FAIpQLScPZJv5Iz9j0uzGd14oGlp_Ioq_4j5zMd_XmdGvI49Bf26jqw/viewform?usp=dialog',
+  poster:  'https://docs.google.com/forms/d/e/1FAIpQLSdI5gLIoP7CciJbT-btP24wmg21gD8ZDRblBbyUCF1mUFNQOA/viewform?usp=dialog',
 };
 
 const EVENT_TITLES = {
